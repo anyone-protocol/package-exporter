87dac6bb1acf32bfe937636ab9c980e199b3ed04
Serhii Varakuta <flood.code@gmail.com>
2024-02-19 03:40:37 +0200

    Expose nginx metrics, handle file truncation

--- internal/fetcher/nginxaccess.go
+++ internal/fetcher/nginxaccess.go
@@ -1,7 +1,10 @@
 package fetcher
 
 import (
+	"bufio"
 	"context"
+	"io"
+	"os"
 	"regexp"
 )
 
@@ -11,6 +14,10 @@ func NewNginxAccessLogFetcher(name, accessLogPath string, accessLogRegexp *regex
 		name:            name,
 		accessLogPath:   accessLogPath,
 		accessLogRegexp: accessLogRegexp,
+
+		logFile:         nil,
+		logReader:       nil,
+		downloadCounter: 0,
 	}
 }
 
@@ -18,13 +25,96 @@ type nginxAccessLogFetcher struct {
 	name            string
 	accessLogPath   string
 	accessLogRegexp *regexp.Regexp
+
+	logFile         *os.File
+	logReader       *bufio.Reader
+	downloadCounter int
 }
 
 // FetchCount fetches download count from nginx access logs and returns it
 func (f *nginxAccessLogFetcher) FetchCount(ctx context.Context) (int, error) {
-	return 0, nil
+	err := f.reopenLogFileIfNeeded()
+	if err != nil {
+		return 0, err
+	}
+
+	for {
+		_, err := f.logReader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				return f.downloadCounter, nil
+			}
+
+			return 0, err
+		}
+
+		f.downloadCounter++
+	}
 }
 
 func (f *nginxAccessLogFetcher) Name() string {
 	return f.name
+}
+
+// reopenLogFileIfNeeded reopens log file if it was truncated or removed
+func (f *nginxAccessLogFetcher) reopenLogFileIfNeeded() error {
+	if f.logFile == nil {
+		return f.openLogFile(0)
+	}
+
+	fileToCheck, err := os.Open(f.accessLogPath)
+	if err != nil {
+		return err
+	}
+
+	defer fileToCheck.Close()
+
+	currentPos, err := f.logFile.Seek(0, io.SeekCurrent)
+	if err != nil {
+		return err
+	}
+
+	fileInfo, err := fileToCheck.Stat()
+	if err != nil {
+		return err
+	}
+
+	// not truncated, early return
+	if currentPos <= fileInfo.Size() {
+		return nil
+	}
+
+	// close previous file handle
+	err = f.logFile.Close()
+	if err != nil {
+		return err
+	}
+
+	// open new file handle at the end
+	err = f.openLogFile(fileInfo.Size())
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// openLogFile opens log file at specified offset
+func (f *nginxAccessLogFetcher) openLogFile(offset int64) error {
+	var err error
+	f.logFile, err = os.Open(f.accessLogPath)
+	if err != nil {
+		return err
+	}
+
+	if offset > 0 {
+		_, err = f.logFile.Seek(offset, io.SeekStart)
+		if err != nil {
+			return err
+		}
+	}
+
+	f.logReader = bufio.NewReader(f.logFile)
+
+	return nil
 }
